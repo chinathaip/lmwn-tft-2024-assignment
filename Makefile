de6ff0ec136c8bebf8e8a5f82afcdd5d52e62311
chinathaip <cartoonabe@gmail.com>
2024-06-25 20:41:45 +0700

    unit test for mapping province + code coverage

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ run:
 
 unit:
 	go clean -testcache && go test -tags=unit -v ./...
+
+unit-race:
+	go test -race -tags=unit -v ./...
+
+unit-coverage:
+	go clean -testcache && go test -tags=unit -coverprofile=coverage.out ./... && go tool cover -html=coverage.out -o coverage.html && open coverage.html
--- data/repository_impl/covid_repository_impl_test.go
+++ data/repository_impl/covid_repository_impl_test.go
@@ -8,47 +8,108 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type covidTest struct {
-	name          string
-	covidData     *dto.CovidData
-	expectedYoung int
-	expectedAdult int
-	expectedOld   int
-	expectedNA    int
-}
-
 func TestSummarize(t *testing.T) {
+	t.Run("Test Mapping Province", func(t *testing.T) {
+		tests := []struct {
+			name        string
+			covidData   *dto.CovidData
+			expectedBkk int
+			expectedNon int
+			expectedCm  int
+		}{
+			{
+				name: "properly handle province",
+				covidData: &dto.CovidData{
+					Data: []dto.CovidCase{
+						{
+							Province: "Bangkok",
+						},
+						{
+							Province: "Bangkok",
+						},
+						{
+							Province: "Nonthaburi",
+						},
+						{
+							Province: "Chiang Mai",
+						},
+					},
+				},
+				expectedBkk: 2,
+				expectedNon: 1,
+				expectedCm:  1,
+			},
+			{
+				name: "empty province should be ignored",
+				covidData: &dto.CovidData{
+					Data: []dto.CovidCase{
+						{
+							Province: "",
+						},
+						{
+							Province: "Bangkok",
+						},
+						{
+							Province: "Nonthaburi",
+						},
+					},
+				},
+				expectedBkk: 1,
+				expectedNon: 1,
+			},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				covidRepoImpl := NewCovidRepositoryImpl(test.covidData)
+				covidSummary := covidRepoImpl.Summarize()
+
+				assert.Equal(t, test.expectedBkk, covidSummary.Province["Bangkok"])
+				assert.Equal(t, test.expectedNon, covidSummary.Province["Nonthaburi"])
+				assert.Equal(t, test.expectedCm, covidSummary.Province["Chiang Mai"])
+			})
+		}
+	})
+
 	t.Run("Test Mapping Age Group", func(t *testing.T) {
-		tests := []covidTest{{
-			name: "properly handle age group",
-			covidData: &dto.CovidData{
-				Data: []dto.CovidCase{
-					{
-						Age: 21,
-					},
-					{
-						Age: 25,
-					},
-					{
-						Age: 55,
-					},
-					{
-						Age: 45,
-					},
-					{
-						Age: 45,
-					},
-					{
-						Age: 90,
-					},
-					{
-						Age: 119,
-					},
-				}},
-			expectedYoung: 2,
-			expectedAdult: 3,
-			expectedOld:   2,
-		},
+		tests := []struct {
+			name          string
+			covidData     *dto.CovidData
+			expectedYoung int
+			expectedAdult int
+			expectedOld   int
+			expectedNA    int
+		}{
+			{
+				name: "properly handle age group",
+				covidData: &dto.CovidData{
+					Data: []dto.CovidCase{
+						{
+							Age: 21,
+						},
+						{
+							Age: 25,
+						},
+						{
+							Age: 55,
+						},
+						{
+							Age: 45,
+						},
+						{
+							Age: 45,
+						},
+						{
+							Age: 90,
+						},
+						{
+							Age: 119,
+						},
+					}},
+				expectedYoung: 2,
+				expectedAdult: 3,
+				expectedOld:   2,
+			},
 			{
 				name: "edge cases should be put in N/A",
 				covidData: &dto.CovidData{
